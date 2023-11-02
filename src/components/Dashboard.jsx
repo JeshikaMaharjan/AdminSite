1313cbb9aaf7a4c16802fea25439a0ec8ef18864
Jeshika Maharjan <jeshika99@gmail.com>
2023-11-02 17:28:29 +0545

    .

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,39 +6,79 @@ import FeedbackIcon from "@mui/icons-material/Feedback";
 import VerifiedUserIcon from "@mui/icons-material/VerifiedUser";
 import logo from "../assets/logo.png";
 export default function Dashboard() {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = months[today.getMonth()];
+  const day = today.getDate();
+
   return (
     <>
       <div className="DashboardContainer">
         <div className="header">
-          <img src={logo} className="logo" />
+          <div className="logo">
+            {/* <h2>kkk</h2> */}
+            <img src={logo} />
+          </div>
           <div className="navbar">
-            <span>To Do</span>
-            <span>Statistics</span>
-            <span>Spam Reports</span>
-            <span>Doctor Relevancy</span>
+            <p>To Do</p>
+            <p>Statistics</p>
+            <p>Spam Reports</p>
+            <p>Doctor Relevancy</p>
           </div>
           <div className="profile">
             <img src={logo} />
+            <h3>Jeshika Maharjan</h3>
+            <p className="logoutbtn">Logout</p>
           </div>
-          <div className="logout">logout</div>
         </div>
         <div className="main">
           <div className="dashboard">
             <div className="dashboardInner">
               <h1>Admin Dashboard</h1>
             </div>
-            <h3 className="date">February 2,2021</h3>
+            <h3 className="date">
+              {month} {day}, {year}
+            </h3>
           </div>
           <div className="todo">
             <div className="todoInner">
               <FormatListBulletedIcon />
               <h2>To Do</h2>
             </div>
+            <div className="circles">
+              <div className="circle">4</div>
+              <span>Spam Reports</span>
+              <div className="circle">5</div>
+              <span>Doctor Relevancy</span>
+            </div>
           </div>
           <div className="statistics">
             <div className="statisticsInner">
               <AssessmentIcon />
               <h2>Statistics</h2>
+            </div>
+            <div className="charts">
+              <div className="graph">
+                <img src="../src/assets/graph.png " />
+              </div>
+              <div className="pieChart">
+                <img src="../src/assets/piechart.png " />
+              </div>
             </div>
           </div>
           <div className="spam">
