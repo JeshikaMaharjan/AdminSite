79685976aa339780f7b876fcc97e176edee88a20
Jeshika Maharjan <jeshika99@gmail.com>
2024-01-14 16:14:07 +0545

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ import Login from "./components/Login";
 import { Route, Routes } from "react-router-dom";
 import ForgotPassword from "./components/ForgotPassword";
 import Dashboard from "./components/Dashboard";
-import GlobalContextProvider from "./context/GlobalStates";
+// import GlobalContextProvider from "./context/GlobalStates";
 
 function App() {
   return (
     <div className="appContainer">
-      <GlobalContextProvider>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-          <Route path="/admin" element={<Dashboard />} />
-        </Routes>
-      </GlobalContextProvider>
+      {/* <GlobalContextProvider> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/admin" element={<Dashboard />} />
+      </Routes>
+      {/* </GlobalContextProvider> */}
     </div>
   );
 }
